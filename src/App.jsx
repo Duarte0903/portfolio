fca3713205b05e4a024a7b59f829313842feccb6
Duarte Leitão <duarte0903@gmail.com>
2023-04-16 15:40:02 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,24 @@ import '../src/style/index.css'
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar></Navbar>
       
+      <div className='hello-card-container'>
+        <div className='hello-card'>
+          <div className='profile-pic-container'>
+            <img src='../public/profile_pic.jpg' className='profile-pic'></img>
+          </div>
+
+          <div className='about-me-container'>
+            <h2>Hello there 👋</h2>
+
+            <p className='about-me-text'>
+              My name is Duarte Leitão and I'm a 20 year old software engineering student
+              at the University of Minho.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
